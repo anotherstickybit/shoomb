0c5d2a985727929058ae7c3e1a589f4073187451
Marat Sadykov <marats93@yandex.ru>
2020-11-26 22:18:53 +0300

    few updates

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,10 +6,11 @@ values ('Metallica'),
 ;
 
 INSERT INTO albums(artist_id, name, img_url)
-VALUES (1, 'Master of Puppets', 'url1'),
-       (1, 'Black Album', 'url2'),
-       (2, 'Back in Black', 'url3'),
-       (3, 'American Idiot', 'url4'),
+VALUES (1, 'Master of Puppets', 'https://upload.wikimedia.org/wikipedia/en/b/b2/Metallica_-_Master_of_Puppets_cover.jpg'),
+       (1, 'Black Album', 'https://upload.wikimedia.org/wikipedia/en/2/2c/Metallica_-_Metallica_cover.jpg'),
+       (1, 'Hardwired…', 'https://upload.wikimedia.org/wikipedia/ru/9/93/Metallica_Hardwired..._To_Self-Destruct_2016.jpeg'),
+       (2, 'Back in Black', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Acdc_backinblack_cover.jpg'),
+       (3, 'American Idiot', 'https://upload.wikimedia.org/wikipedia/ru/c/c0/Greenday_americanidiot.png'),
        (4, 'Dragons1', 'url5')
 ;
 
@@ -32,7 +33,19 @@ values ('Battery', 1),
        ('Of Wolf & Man', 2),
        ('The God That Failed', 2),
        ('My Friend of Misery', 2),
-       ('The Struggle Within', 2)
+       ('The Struggle Within', 2),
+       ('Hardwired', 3),
+       ('Atlas, Rise!', 3),
+       ('Now That We''re Dead', 3),
+       ('Moth Into Flame', 3),
+       ('Dream No More', 3),
+       ('Halo On Fire', 3),
+       ('Confusion', 3),
+       ('ManUNkind', 3),
+       ('Here Comes Revenge', 3),
+       ('Am I Savage?', 3),
+       ('Murder One', 3),
+       ('Spit Out The Bone', 3)
 ;
 
 insert into playlists(name)
